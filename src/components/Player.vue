39fc982b6b80d38bb73a807b1a131f2828067f6f
Qingxiang Jia <ybjqx3340@gmail.com>
2018-09-14 17:15:16 -0700

    Add listeners to the buttons

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,14 +1,25 @@
 <template>
   <div id='player'>
-    <button>Play</button>
-    <button>Pause</button>
-    <button>Stop</button>
+    <button v-on:click='playClicked'>Play</button>
+    <button v-on:click='pauseClicked'>Pause</button>
+    <button v-on:click='stopClicked'>Stop</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "player"
+  name: 'player',
+  methods: {
+    playClicked: function (event) {
+      console.log('play clicked!')
+    },
+    pauseClicked: function (event) {
+      console.log('pause clicked!')
+    },
+    stopClicked: function (event) {
+      console.log('stop clicked!')
+    }
+  }
 };
 </script>
 
