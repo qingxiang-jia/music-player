cf9adb0fcfbd76b9c01d56d637ea6f76af068adc
Qingxiang Jia <ybjqx3340@gmail.com>
2018-09-16 18:17:29 -0700

    Get the basic playlist done

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios';
+import XmlReader from 'xml-reader';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    playlist: ''
+    playlist: []
   },
   mutations: {
     setPlaylist (state, newList) {
@@ -17,7 +18,15 @@ export default new Vuex.Store({
     fetchPlaylist (context) {
       axios.get('http://127.0.0.1:8081/')
       .then(function (response) {
-        context.commit('setPlaylist', response.data)
+        let reader = XmlReader.create()
+        let newList = []
+        reader.on('done', data => {
+          for (let element of data.children) {
+            newList.push({url: element.attributes.href, filename: element.children[0].value})
+          }
+        })
+        reader.parse(response.data)
+        context.commit('setPlaylist', newList)
       })
       .catch(function (error) {
         console.log('Failed to request playlist:', error)
