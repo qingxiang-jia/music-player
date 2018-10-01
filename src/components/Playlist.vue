8827ffb5db2c34f4dd650cb7b737ad0e5a7ba913
Qingxiang Jia <ybjqx3340@gmail.com>
2018-09-30 17:47:52 -0700

    Remove unused code and comment

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,11 +1,6 @@
 <template>
   <div id='playlist'>
     <p>Playlist</p>
-    <!-- <ul>
-      <li v-for='song in songs'>
-        <span v-bind:href='song.url' v-on:click='songSelected'>{{ song.filename }}</span>
-      </li>
-    </ul> -->
     <div>Selected song: {{ nowPlaying }}</div>
     <select size='20' v-model='nowPlaying'>
       <option v-for='song in songs' v-bind:value='song'>
@@ -23,19 +18,6 @@ export default {
   name: "playlist",
   created: function() {
     this.$store.dispatch("fetchPlaylist");
-  },
-  data: function() {
-    return {
-      selectedSong: null
-    };
-  },
-  methods: {
-    songSelected: function(event) {
-      this.$store.commit("setNowPlaying", {
-        url: event.target.attributes.href.value,
-        filename: event.target.innerHTML
-      });
-    }
   },
   computed: {
     songs() {
